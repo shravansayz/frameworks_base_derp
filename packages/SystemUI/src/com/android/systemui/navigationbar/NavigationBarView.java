6fcfb243c0baacd0debfabfe6183bd2ac3a4b63c
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: Back gesture height settings [1/2]
    idoybh (YAAP):
    * Adapted
    * Improved code and logic
    * Improved docs, typos etc
    * Use sixths instead of fixed values, add 2 more steps
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -65,6 +65,7 @@ import com.android.app.animation.Interpolators;
 import com.android.app.viewcapture.ViewCaptureFactory;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settingslib.Utils;
+import com.android.systemui.Dependency;
 import com.android.systemui.Gefingerpoken;
 import com.android.systemui.model.SysUiState;
 import com.android.systemui.navigationbar.buttons.ButtonDispatcher;
@@ -99,7 +100,8 @@ import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 
 /** */
-public class NavigationBarView extends FrameLayout {
+public class NavigationBarView extends FrameLayout
+        implements NavigationModeController.ModeChangedListener {
     final static boolean DEBUG = false;
     final static String TAG = "NavBarView";
 
@@ -353,6 +355,9 @@ public class NavigationBarView extends FrameLayout {
                 setNavigationIconHints(mNavigationIconHints);
             }
         };
+
+        final NavigationModeController controller = Dependency.get(NavigationModeController.class);
+        controller.addListener(this);
     }
 
     public void setEdgeBackGestureHandler(EdgeBackGestureHandler edgeBackGestureHandler) {
@@ -874,6 +879,14 @@ public class NavigationBarView extends FrameLayout {
         mRotationButtonController.onNavigationModeChanged(mNavBarMode);
         updateRotationButton();
     }
+
+    @Override
+    public void onNavigationHeightChanged() {
+        mEdgeBackGestureHandler.onNavigationHeightChanged();
+    }
+
+    @Override
+    public void onNavigationModeChanged(int mode) { /* Do nothing */ }
 
     public void setAccessibilityButtonState(final boolean visible, final boolean longClickable) {
         mLongClickableAccessibilityButton = longClickable;
