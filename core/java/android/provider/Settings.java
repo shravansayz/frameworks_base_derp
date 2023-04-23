3c015f3f247b6f7e4424f7dfcdcddca1e4736cc4
SamarV-121 <samarvispute121@pm.me>
2023-04-23 13:44:56 +0530

    SystemUI: Add brightness slider styles [1/3]
    Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13140,6 +13140,13 @@ public final class Settings {
         public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
 
         /**
+         * Brightness slider styles
+         * @hide
+         */
+        @SuppressLint("UnflaggedApi")
+        public static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -547,6 +547,30 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 },
                 UserHandle.USER_ALL);
 
+        mSecureSettings.registerContentObserverForUserSync(
+                Settings.Secure.getUriFor(Settings.Secure.BRIGHTNESS_SLIDER_STYLE),
+                false, new ContentObserver(mBgHandler) {
+                    @Override
+                    public void onChange(
+                            boolean selfChange, Collection<Uri> collection, int flags, int userId) {
+                        if (DEBUG)
+                            Log.d(TAG, "Overlay changed for user: " + userId);
+                        if (mUserTracker.getUserId() != userId) {
+                            return;
+                        }
+                        if (!mDeviceProvisionedController.isUserSetup(userId)) {
+                            Log.i(TAG, "Theme application deferred when setting changed.");
+                            mDeferredThemeEvaluation = true;
+                            return;
+                        }
+                        int brightnessSliderStyle =
+                                Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                                        Settings.Secure.BRIGHTNESS_SLIDER_STYLE, 0,
+                                        UserHandle.USER_CURRENT);
+                        mThemeManager.setBrightnessSliderStyle(brightnessSliderStyle);
+                    }
+                }, UserHandle.USER_ALL);
+
         // All wallpaper color and keyguard logic only applies when Monet is enabled.
         if (!mIsMonetEnabled) {
             return;
