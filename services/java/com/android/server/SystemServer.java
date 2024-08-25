863b414095f6ef829528f409101bffbe84195d57
Ido Ben-Hur <idoybh2@gmail.com>
2024-08-25 14:58:21 +0300

    base: Make PowerOffAlarmService opt-in
    noob maintainers be noob. force em to check it's working and enable the config.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1730,9 +1730,11 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(LogcatManagerService.class);
             t.traceEnd();
 
-            t.traceBegin("StartPowerOffAlarmService");
-            mSystemServiceManager.startService(PowerOffAlarmService.class);
-            t.traceEnd();
+            if (context.getResources().getBoolean(R.bool.config_powerOffAlarmEnabled)) {
+                t.traceBegin("StartPowerOffAlarmService");
+                mSystemServiceManager.startService(PowerOffAlarmService.class);
+                t.traceEnd();
+            }
 
         } catch (Throwable e) {
             Slog.e("System", "******************************************");
