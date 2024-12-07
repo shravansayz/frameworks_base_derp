LeafOS-Project/android_frameworks_base

gmscompat: Un-spoof GMS while adding google account
GMS uses its DroidGuard service (the same that's used for safetynet)
to get the device name from the build fingerprint, which from our
current certified props, is Pixel XL. The device name after Google
login shows up in the login alert accordingly as Pixel XL, which is
inconvenient especially when you have multiple devices on the same ROM.

By restarting the DroidGuard service process (gms.unstable) as soon as
the user starts adding Google account (i.e when the GMS add account
activity is started and moved to the top of the task stack), we can
skip spoofing to certified props in this instance. When the user
stops/finishes adding account (GMS add account activity is no longer
at the top of the task stack), restart the process again, this time
spoofing to certified props in order to be ready for safetynet.

This ensures while adding a Google account, GMS reads the original
properties of the device and labels the device with its original name,
while in any other situation GMS reads certified properties required
to pass SafetyNet/Play Integrity.

Test:
- Add google account from setup wizard, settings, any other app
- Observe correct device name in gmail login alert
- SafetyNet (basic, CTS), Play Integrity (basic, device) passes.

Change-Id: Ia5463f220a8262f7963fc5b2640a7a5311a73210

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -270,6 +270,8 @@ import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 
+import com.android.internal.gmscompat.AttestationHooks;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1965,7 +1967,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -2995,7 +2999,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 

shravansayz/frameworks_base_derp

Change-Id: Ifc9bd4d4295cfb7c0eaad6172e12f88c22d6ec26
Signed-off-by: SamarV-121 <samar@samarv121.dev>

services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -292,6 +292,8 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 
+import com.android.internal.gmscompat.AttestationHooks;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1996,7 +1998,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3132,7 +3136,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!AttestationHooks.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 